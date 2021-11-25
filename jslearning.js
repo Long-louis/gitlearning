304e5227b3909c311717d624e8e6a4e23b32dda1
Long_Surface <2575090023@qq.com>
2021-11-25 10:42:30 +0800

    p87 js

--- jslearning.js
+++ jslearning.js
@@ -1,8 +1,12 @@
-var str = "1a2b3c4d";
-var result = str.split(/[a-z]/);
-str = "hello bc hello aec";
-str = "1a2b3cc4d5er";
-result = str.match(/[A-z]/);
+// var str = "1a2b3c4d";
+// var result = str.split(/[a-z]/);
+// str = "hello bc hello aec";
+// str = "1a2ba3cc4d5eAr";
+// result = str.match(/[a-z]/gi);
+
 // result = str.search(/a[be]c/);
-console.log( Array.isArray(result));
+// result = str.replace(/[A-z]/gi, "");
+var reg = /aaa/;
+
+console.log(result);
 
